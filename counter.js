bfb21d4b56b8a64828cdc9f0c527c79a3ecfbfda
JevGor <reawqk@mail.ru>
2020-01-27 14:46:56 +0100

    Create counter.js

---
+++ counter.js
@@ -0,0 +1,20 @@
+var counter = 15
+
+function div() {
+    if (counter >= 1 && counter <= 15) {
+        counter = counter - 1
+    } else {
+        console.log('counter not in sfram')
+    }
+}
+
+div()
+
+function add() {
+    if (counter >= 0 && counter <= 14) {
+        counter = counter + 1
+    } else {
+        console.log('counter not in sfram')
+    }
+}
+add()
